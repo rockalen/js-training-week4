54840bac5e0e0c66c4a165b3ce18821b09d10c82
rockalen <rockalen0523@gmail.com>
2020-07-19 18:03:50 +0800

    money 千分號轉換

--- assets/script/all.js
+++ assets/script/all.js
@@ -1,4 +1,9 @@
-
+// money，千分號轉換
+Vue.filter('money', function(num) {
+    const parts = num.toString().split('.');
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    return '$' + parts.join('.');
+});
 new Vue({
     el: '#app',
     data: {
